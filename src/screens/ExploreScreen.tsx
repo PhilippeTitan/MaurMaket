44f23537b055c8ba3ce46e0d1a12570809e647c6
PhilippeTitan <philijanathethird@gmail.com>
2026-06-30 18:56:48 -0400

    fix: avatar size 35px everywhere

--- src/screens/ExploreScreen.tsx
+++ src/screens/ExploreScreen.tsx
@@ -195,7 +195,7 @@ export default function ExploreScreen({ navigation }: Props) {
           <View style={styles.cardInfoBar}>
             {item.seller && (
               <View style={styles.cardSellerRow}>
-                <UserAvatar seller={item.seller} size={50} />
+                <UserAvatar seller={item.seller} size={35} />
                 <Text style={styles.cardName} numberOfLines={1}>{item.name}</Text>
               </View>
             )}
